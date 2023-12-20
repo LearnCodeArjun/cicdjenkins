456bbd25eea7812e09fcd7f1ffd8cefe77967dd0
Arjun <123224576+LearnCodeArjun@users.noreply.github.com>
2023-12-20 22:16:59 +0530

    adding employee class

---
+++ Employee.java
@@ -0,0 +1,8 @@
+public class Employee{
+    publiv Employee(){
+        System.out.println("creating employee object");
+    }
+    public void sayHii(){
+        System.out.println("hii i am employee");
+    }
+}
--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main{
        System.out.println(new Date());
         System.out.println("hii");
         System.out.println("add one more profile");
+        Employee emp=new Employee();
+        emp.sayHii();
         
     }
 }
