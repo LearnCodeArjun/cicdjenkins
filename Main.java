215f4ccb8bbbe52c67e6f5eab6989c3ddbae2918
Arjun <123224576+LearnCodeArjun@users.noreply.github.com>
2023-12-20 16:20:05 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("hii this is first project");
        System.out.println(new Date());
+        System.out.println("hii");
     }
 }
